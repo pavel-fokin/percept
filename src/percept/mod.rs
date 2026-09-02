8d9fcc483e62f704cb7519ad4caa82aad94545bb
Pavel Fokin <me@pavelfokin.dev>
2026-09-02 10:45:17 +0200

    refactor: rename src/message.rs to src/model.rs

--- src/percept/mod.rs
+++ src/percept/mod.rs
@@ -1,9 +1,9 @@
 mod event;
 mod event_log;
-mod message;
+mod model;
 mod search;
 
 pub use event::{Actor, Event, EventId, EventKind, Payload};
 pub use event_log::EventLog;
-pub use message::{to_messages, Chunk, Message, Model, ReplyStream};
+pub use model::{to_messages, Chunk, Message, Model, ReplyStream};
 pub use search::{EventQuery, EventSearch};
